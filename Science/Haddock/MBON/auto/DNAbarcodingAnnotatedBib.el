9aba2dc17f9447d6494ea254784442bcd937aa7d
Darrin <darrin.schultz91@gmail.com>
2015-02-19 16:45:34 -0800

    Added MaxMSP patches, Arduino code, and python stuff

---
+++ Science/Haddock/MBON/auto/DNAbarcodingAnnotatedBib.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook "DNAbarcodingAnnotatedBib"
+ (lambda ()
+    (TeX-add-symbols
+     "HRule"
+     "Hrule")
+    (TeX-run-style-hooks
+     "parskip"
+     ""
+     "geometry"
+     "bottom=1in"
+     "top=0.7in"
+     "right=1in"
+     "left=1in"
+     "latex2e"
+     "art12"
+     "article"
+     "12pt")))
+
---
+++ Science/Haddock/codingAC/ac.py
@@ -0,0 +1,9 @@
+def main():
+    thematrix= [1,2,3,4]
+    length = input( "Enter the length: " )
+    width = input( "Enter the width: " )
+    area = length * width
+    print( "The area is " + str(area) )
+    print thematrix
+
+main()
