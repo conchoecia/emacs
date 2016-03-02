cb7ea8ddf266e52803a37c074fcffd7b7c4d887e
cypridina <darrin.schultz91@gmail.com>
2016-03-01 20:05:51 -0800

    still not working

---
+++ .emacs.d/SessionDesktop.el
@@ -0,0 +1,21 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Tue Jan 27 09:42:08 2015
+;; Desktop file format version 206
+;; Emacs version 24.4.51.2
+(setq revive:frame-configuration-to-restore
+  '(progn ))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (21703 52720 184238 0) (desktop . "206") "dschultz@Darrins-MBP.att.net" nil nil ((((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility) (frameset--id . "CAF1-308C-C691-2EA6") (frameset--mini t . t) (height . 47) (width . 98) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "Darrins-MBP.att.net") (explicit-name . t) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 7) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 32) (min-pixel-width-ignore . 43) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 715) (pixel-height . 736) (total-width . 102) (total-height . 46) (normal-height . 1.0) (normal-width . 1.0) (buffer " *empty*" (selected . t) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/Science/Haddock/lab_notebook/DTS_Haddock_lab_book.tex"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
